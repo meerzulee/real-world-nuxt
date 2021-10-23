396fd2c7c12c0bafac30a3741895c34e625f963b
Meerzulee <meerzulee@gmail.com>
2021-10-24 01:54:43 +0600

    Procfile config and nuxt target:static

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
-
+  target: 'static',
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
   loading: {
